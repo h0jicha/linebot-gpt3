ccbf87b7eaf1afa0fa90d9ce7f3d76dad338765d
tnbnt <info@charr.dev>
2022-01-31 02:55:53 +0900

    load tokens from environment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os
 
 from linebot import (
    LineBotApi, WebhookHandler
@@ -11,12 +12,16 @@ from linebot.models import (
 )
 import openai
 
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_OPENAI_API_KEY = os.environ["YOUR_OPENAI_API_KEY"]
+
+openai.api_key = YOUR_OPENAI_API_KEY
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello_world():
