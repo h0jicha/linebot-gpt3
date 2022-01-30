60283207bb72089c51a3c85acd791dd0fe69811b
tnbnt <info@charr.dev>
2022-01-31 02:39:01 +0900

    set gpt-3 responce

--- main.py
+++ main.py
@@ -1,63 +1,61 @@
-# 必要モジュールの読み込み
 from flask import Flask, request, abort
-import os
+
 from linebot import (
-    LineBotApi, WebhookHandler
+   LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+   InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+   MessageEvent, TextMessage, TextSendMessage,
 )
+import openai
 
-# 変数appにFlaskを代入。インスタンス化
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
 app = Flask(__name__)
 
-# 環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-# Herokuログイン接続確認のためのメソッド
-# Herokuにログインすると「hello world」とブラウザに表示される
 @app.route("/")
 def hello_world():
     return "hello world!"
 
-# ユーザーからメッセージが送信された際、LINE Message APIからこちらのメソッドが呼び出される。
 @app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得
-    signature = request.headers['X-Line-Signature']
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
 
-    # リクエストボディを取得
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
 
-    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
-    try:
-        handler.handle(body, signature)
-    # 署名検証で失敗した場合、例外を出す。
-    except InvalidSignatureError:
-        abort(400)
-    # handleの処理を終えればOK
-    return 'OK'
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       print("Invalid signature. Please check your channel access token/channel secret.")
+       abort(400)
 
-# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-# def以下の関数を実行します。
-# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+   return 'OK'
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    res = openai.Completion.create(
+       engine="davinci",
+       prompt=f"日本語AIチャットボット\n日本人の質問をAIが日本語で答えます\nHuman:{event.message.text}\nAI:",
+       temperature=0.9,
+       max_tokens=150,
+       presence_penalty=0.6,
+       stop=["\n", "Human:", "AI:"]
+   )
 
-# ポート番号の設定
+    line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=res["choices"][0]["text"]))
+
+
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+   app.run()
