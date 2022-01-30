f53beab3acbf7bfbb08e2b68bd1edec033668670
tnbnt <info@charr.dev>
2022-01-31 04:12:54 +0900

    modify main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, abort
 import os
 
@@ -13,12 +14,16 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get('YOUR_CHANNEL_ACCESS_TOKEN')
-YOUR_CHANNEL_SECRET = os.environ.get('YOUR_CHANNEL_SECRET')
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -33,11 +38,9 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -45,6 +48,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
 if __name__ == "__main__":
-    app.run()
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
